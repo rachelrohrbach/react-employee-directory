0f8e2b1447fed3475035791d8d194794dbca7e32
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-04-11 00:06:01 -0700

    added Results to be displayed

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import React from 'react';
+import Results from './components/Results'
 
 function App() {
+
   return (
     <div className="App">
       <Header />
+      <Results />
     </div>
   );
 }
