3d3cdff1276913125fbafa36123e6fdb5cf63a73
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-04-12 02:11:48 -0700

    added wrapper and container components

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Header from './components/Header';
 import React from 'react';
-import Results from './components/Results'
+import Results from './components/Results';
+import Container from './components/Container';
 
 function App() {
-
   return (
     <div className="App">
       <Header />
-      <Results />
+      <Container>
+        <Results />
+      </Container>
     </div>
   );
 }
