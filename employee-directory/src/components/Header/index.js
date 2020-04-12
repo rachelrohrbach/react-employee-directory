b530bb093953cde36f8b7adcdb3812bfee40a36a
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-04-12 16:30:46 -0700

    updated instructions for sorting and filtering directory

--- employee-directory/src/components/Header/index.js
+++ employee-directory/src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header () {
     return (
       <div className="header">
         <h1>Employee Directory</h1>
-        <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+        <p>Click on the Name or DOB headers to sort by descending order or use the search boxes to narrow your results.</p>
       </div>
     )
   }
